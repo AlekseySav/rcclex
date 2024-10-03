10fd6f2c55e43fc9dd7cb7fb7d1087cfcfcaae89
schetvkomplax <schet.v.komplax@gmail.com>
2024-10-03 11:18:46 +0300

    add yaml

--- build.zig
+++ build.zig
@@ -2,17 +2,12 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-
     const optimize = b.standardOptimizeOption(.{});
 
-    const lib = b.addStaticLibrary(.{
-        .name = "rcclex",
-        .root_source_file = b.path("src/root.zig"),
+    const yaml = b.dependency("yaml", .{
         .target = target,
         .optimize = optimize,
     });
-
-    b.installArtifact(lib);
 
     const exe = b.addExecutable(.{
         .name = "rcclex",
@@ -20,12 +15,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
+    exe.root_module.addImport("yaml", yaml.module("yaml"));
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
-
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
@@ -33,23 +28,13 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
+    exe_unit_tests.root_module.addImport("yaml", yaml.module("yaml"));
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
     test_step.dependOn(&run_exe_unit_tests.step);
 }
